15b7f9e43575315faaf4ddedc6e8625fc2a848d6
jmlippincott <82314566+jmlippincott@users.noreply.github.com>
2021-07-26 05:13:42 -0400

    Delete 01_capital_indexes.py

--- 01_capital_indexes.py
+++
@@ -1,8 +0,0 @@
-# Write a function named capital_indexes. The function takes a single parameter, which is a string. Your function should return a list of all the indexes in the string that have capital letters.
-#
-# For example, calling capital_indexes("HeLlO") should return the list [0, 2, 4].
-
-def capital_indexes(word):
-    word = list(word)
-    capitals = [count for count,ele in enumerate(word) if ele.isupper()]
-    return capitals
