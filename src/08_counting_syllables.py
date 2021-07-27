02509d47b948fc05400dd4cfd2a7c188be4a7f43
jmlippincott <joshua.m.lippincott@gmail.com>
2021-07-27 18:44:44 -0400

    added 08_count_syllables

---
+++ src/08_counting_syllables.py
@@ -0,0 +1,12 @@
+# Define a function named count that takes a single parameter. The parameter is a string. The string will contain a single word divided into syllables by hyphens, such as these:
+#
+# "ho-tel"
+# "cat"
+# "met-a-phor"
+# "ter-min-a-tor"
+# Your function should count the number of syllables and return it.
+#
+# For example, the call count("ho-tel") should return 2.
+
+def count(word):
+    return len([count for count,elem in enumerate(word) if elem == "-"]) + 1
